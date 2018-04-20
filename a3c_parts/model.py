695e4c7c44988dc6aeac57adf0539ddc55c1c6de
ilya.sergeev <ilya.sergeev@corp.mail.ru>
2018-04-20 20:11:33 +0300

    actor critic

---
+++ a3c_parts/model.py
@@ -0,0 +1,29 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ActorCritic(torch.nn.Module):
+    def __init__(self, num_inputs, action_space):
+        super(ActorCritic, self).__init__()
+        self.conv1 = nn.Conv2d(num_inputs, 32, 3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+
+        num_outputs = action_space.n
+        self.critic_linear = nn.Linear(32*3*3, 1)
+        self.actor_linear = nn.Linear(32*3*3, num_outputs)
+        self.train()
+
+    def forward(self, inputs):
+        print(inputs)
+        x = F.elu(self.conv1(inputs))
+        x = F.elu(self.conv2(x))
+        x = F.elu(self.conv3(x))
+        x = F.elu(self.conv4(x))
+
+        x = x.view(-1, 32 * 3 * 3)
+
+        return self.critic_linear(x), self.actor_linear(x)
