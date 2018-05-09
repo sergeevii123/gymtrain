4f64a8ff78a2c73389c7a9fe71d850ddbcfae317
ilya.sergeev <ilya.sergeev@corp.mail.ru>
2018-05-09 18:24:22 +0300

    actor critic

--- actor_critic.py
+++ actor_critic.py
@@ -20,7 +20,7 @@ parser.add_argument('--seed', type=int, default=42, metavar='N',
                     help='random seed (default: 1)')
 parser.add_argument('--render', action='store_true',
                     help='render the environment')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+parser.add_argument('--log-interval', type=int, default=1, metavar='N',
                     help='interval between training status logs (default: 10)')
 parser.add_argument('--cont', action='store_true',
                     help='continue from weights')
@@ -63,6 +63,30 @@ class Policy(nn.Module):
         action_scores = self.action_head(x)
         state_values = self.value_head(x)
         return F.softmax(action_scores, dim=-1), state_values
+
+
+# class Policy(nn.Module):
+#     def __init__(self, num_inputs, action_space):
+#         super(Policy, self).__init__()
+#         self.conv1 = nn.Conv2d(num_inputs, 32, 8, stride=4, padding=1)
+#         self.conv2 = nn.Conv2d(32, 64, 4, stride=2, padding=1)
+#         self.conv3 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
+#         self.affine1 = nn.Linear(6400, 512)
+#         self.action_head = nn.Linear(512, action_space)
+#         self.value_head = nn.Linear(512, 1)
+#
+#         self.saved_actions = []
+#         self.rewards = []
+#
+#     def forward(self, x):
+#         x = F.elu(self.conv1(x))
+#         x = F.elu(self.conv2(x))
+#         x = F.elu(self.conv3(x))
+#         x = x.view(-1, 6400)
+#         x = F.elu(self.affine1(x))
+#         action_scores = self.action_head(x)
+#         state_values = self.value_head(x)
+#         return F.softmax(action_scores, dim=-1), state_values
 
 
 model = Policy(env.observation_space.shape[0], env.action_space.n)
