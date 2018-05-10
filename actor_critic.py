78d58cdc8ec50eac0ad69bc0ce1d1e693f19728e
ilya.sergeev <ilya.sergeev@corp.mail.ru>
2018-05-10 16:06:21 +0300

    actor critic

--- actor_critic.py
+++ actor_critic.py
@@ -71,7 +71,7 @@ class Policy(nn.Module):
         self.conv1 = nn.Conv2d(num_inputs, 32, 8, stride=4, padding=1)
         self.conv2 = nn.Conv2d(32, 64, 4, stride=2, padding=1)
         self.conv3 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
-        self.affine1 = nn.Linear(6400, 512)
+        self.affine1 = nn.Linear(640, 512)
         self.action_head = nn.Linear(512, action_space)
         self.value_head = nn.Linear(512, 1)
 
@@ -82,7 +82,7 @@ class Policy(nn.Module):
         x = F.elu(self.conv1(x))
         x = F.elu(self.conv2(x))
         x = F.elu(self.conv3(x))
-        x = x.view(-1, 6400)
+        x = x.view(-1, 640)
         x = F.elu(self.affine1(x))
         action_scores = self.action_head(x)
         state_values = self.value_head(x)
